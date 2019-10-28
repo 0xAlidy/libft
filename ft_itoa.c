e4691ac103ebfa3101482b4a1b5bd9f5e99409b5
Axel Lidy <alidy@z1r8p4.le-101.fr>
2019-10-28 15:30:18 +0100

    commit avant de push

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: alidy <alidy@student.le-101.fr>            +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 15:10:07 by alidy        #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/17 17:02:36 by alidy       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/28 15:20:24 by alidy       ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,7 +19,7 @@ static	int		nb_char(int n)
 
 	if (n == 0)
 		return (1);
-	if (n < 0)
+	else if (n < 0)
 	{
 		res = 1;
 		n = n * -1;
@@ -41,24 +41,6 @@ static	int		ft_neg(int n)
 	return (0);
 }
 
-static	char	*ft_int_min(char *s)
-{
-	int		min;
-	int		i;
-
-	min = 147483648;
-	i = 10;
-	s[0] = '-';
-	s[1] = '2';
-	while (i > 1)
-	{
-		s[i] = (min % 10 + 48);
-		min = min / 10;
-		i--;
-	}
-	return (s);
-}
-
 char			*ft_itoa(int n)
 {
 	int		nb_c;
@@ -69,10 +51,10 @@ char			*ft_itoa(int n)
 	nb_c = nb_char(n);
 	i = nb_c - 1;
 	neg = ft_neg(n);
+	if (n == -2147483648)
+		return (str = ft_strdup("-2147483648"));
 	if ((str = malloc(sizeof(char) * (nb_c + 1))) == NULL)
 		return (0);
-	if (n == -2147483648)
-		return (ft_int_min(str));
 	str[nb_c] = 0;
 	if (neg == 1)
 		n = n * -1;
